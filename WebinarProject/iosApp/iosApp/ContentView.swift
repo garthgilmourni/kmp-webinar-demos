13b843f9d02546bbfa3ec661291f2d4fd00f5641
Garth Gilmour <garth.gilmour@jetbrains.com>
2023-11-13 17:44:48 +0000

    Add transition to Weather Detail view

--- WebinarProject/iosApp/iosApp/ContentView.swift
+++ WebinarProject/iosApp/iosApp/ContentView.swift
@@ -2,9 +2,21 @@ import SwiftUI
 import Shared
 
 
+struct WeatherDetails {
+    let part1: String
+    let part2: String
+}
+
 struct CardView: View {
     let countryImage: String
     let countryDescription: String
+    let action: (WeatherDetails) -> ()
+
+    init(countryImage: String, countryDescription: String, action: @escaping (WeatherDetails) -> ()) {
+        self.countryImage = countryImage
+        self.countryDescription = countryDescription
+        self.action = action
+    }
 
     var body: some View {
         ZStack {
@@ -18,22 +30,70 @@ struct CardView: View {
                         .foregroundColor(.accentColor)
                 Text(countryImage)
                 Text(countryDescription)
+                Button("Push Me") {
+                    action(WeatherDetails(part1: countryImage, part2: "Sunny"))
+                }
             }
-        }
-                .frame(width: 300, height: 200)
+        }.frame(width: 300, height: 200)
     }
 }
 
-
 struct ContentView: View {
+    @State private var showDetailsView = false
+    @State private var weatherDetails: WeatherDetails?
+
+    var body: some View {
+        if (showDetailsView) {
+            WeatherView(details: weatherDetails.unsafelyUnwrapped) {
+                withAnimation {
+                    showDetailsView.toggle()
+                }
+            }.transition(.scale)
+        } else {
+            CountriesView { details in
+                withAnimation {
+                    showDetailsView.toggle()
+                    weatherDetails = details
+                }
+            }
+        }
+    }
+}
+
+struct CountriesView: View {
+    let action: (WeatherDetails) -> ()
+
+    init(action: @escaping (WeatherDetails) -> ()) {
+        self.action = action
+    }
+
     var body: some View {
         ScrollView {
-            LazyVStack(alignment: .leading) {
+            LazyVStack {
                 ForEach(1...20, id: \.self) {
-                    CardView(countryImage: "Ireland\($0)", countryDescription: "The emerald isle")
+                    CardView(countryImage: "Ireland\($0)", countryDescription: "The emerald isle", action: action)
                 }
             }
         }.padding()
+    }
+}
+
+struct WeatherView: View {
+
+    let details: WeatherDetails
+    let action: () -> ()
+
+    init(details: WeatherDetails, action: @escaping () -> ()) {
+        self.details = details
+        self.action = action
+    }
+
+    var body: some View {
+        Text("It is \(details.part2) in \(details.part1)")
+        Button("Push Me") {
+            action()
+        }
+        Spacer()
     }
 }
 
